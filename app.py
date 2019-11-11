1094f0670c74b698a9dc4538e239bc66355b44f5
pc <eugenezaharchenko@gmail.com>
2019-11-12 00:23:10 +0200

    main page added

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask
+from config import Configuration
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app.config.from_object(Configuration)
 
 
 if __name__ == '__main__':
