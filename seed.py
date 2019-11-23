a87aebf91daec60977759ef37e38253fc72f2998
Dan Pashchenko <bogdan.pashchenko@gmail.com>
2019-11-24 01:59:11 +0200

    minor

--- seed.py
+++ seed.py
@@ -4,8 +4,10 @@ from app import bcrypt
 
 
 def seed():
-    if User.query.count() != 0:
-        print(User.query.count())
+    users_count = User.query.count()
+    print(f'Users count: {users_count}')
+
+    if users_count != 0:
         return None
 
     user1 = User(username='obama', password_hash=str(bcrypt.generate_password_hash('123')))
